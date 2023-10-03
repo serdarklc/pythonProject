c7feb21bf5da55f5cf09399f1ebb5b44d1ac210b
Serdar <srdrklc13@gmail.com>
2023-10-03 22:04:54 +0300

    for loop modules are created

---
+++ day02/loops.py
@@ -0,0 +1,25 @@
+
+s = 'Python'
+
+for each in s:
+    print(each)
+
+print('-----------------------')
+
+for i in range(0,len(s)):
+    print(s[i])
+
+print('-----------------------')
+
+for i in range(-len(s),0):
+    print(s[i])
+
+print('-----------------------')
+
+for i in range(len(s)):
+    print(i)
+
+print('-----------------------')
+
+for i in reversed(range(0,len(s))):
+    print(s[i])
